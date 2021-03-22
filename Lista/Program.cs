7a5a2a6ee8ec5fe65dfb9250c91e033eb6e1cea4
Carlos <Carlos@DESKTOP-BKJM8M2>
2021-03-22 15:31:41 -0300

    Alguns exemplos de manipulação em lista usando o C#

--- Lista/Program.cs
+++ Lista/Program.cs
@@ -13,6 +13,9 @@ namespace Lista
             list.Insert(0, "Marco");
             list.Add("Alex");
             list.Insert(1, "Anna");
+            list.Add("André");
+            list.Add("José");
+            list.Add("Carlos");
 
             foreach (string obj in list)
             {
@@ -25,6 +28,54 @@ namespace Lista
 
             string s2 = list.FindLast(x => x[0] == 'A');
             Console.WriteLine("Ultima ocorrencia de nome iniciado com a letra A: " + s2);
+
+            int pos1 = list.FindIndex(x => x[0] == 'A');
+            Console.WriteLine("Primeira posição de quem começa com A: " + pos1);
+
+            int pos2 = list.FindLastIndex(x => x[0] == 'A');
+            Console.WriteLine("Primeira posição de quem começa com A: " + pos2);
+
+            List<string> list2 = list.FindAll(x => x.Length == 5);
+            Console.WriteLine("-------------");
+
+            foreach (string obj in list2)
+            {
+                Console.WriteLine(obj);
+            }
+
+            list.Remove("Alex");
+            Console.WriteLine("-------------");
+
+            foreach (string obj in list)
+            {
+                Console.WriteLine(obj);
+            }
+
+            list.RemoveAll(x => x[0] == 'M');
+            Console.WriteLine("-------------");
+
+            foreach (string obj in list)
+            {
+                Console.WriteLine(obj);
+            }
+
+            list.RemoveAt(1);
+            Console.WriteLine("-------------");
+
+            foreach (string obj in list)
+            {
+                Console.WriteLine(obj);
+            }
+
+            list.RemoveAt(1);
+
+            list.RemoveRange(0, 1);
+            Console.WriteLine("-------------");
+
+            foreach (string obj in list)
+            {
+                Console.WriteLine(obj);
+            }
         }
     }
 }
