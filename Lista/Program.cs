172a3294589c6ce7e272251d4348b2ec85bea40d
Carlos <Carlos@DESKTOP-BKJM8M2>
2021-03-22 15:15:09 -0300

    Teste

--- Lista/Program.cs
+++ Lista/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Lista
 {
@@ -6,7 +7,24 @@ namespace Lista
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<string> list = new List<string>();
+
+            list.Add("Maria");
+            list.Insert(0, "Marco");
+            list.Add("Alex");
+            list.Insert(1, "Anna");
+
+            foreach (string obj in list)
+            {
+                Console.WriteLine(obj);
+            }
+            Console.WriteLine("List.Count: " + list.Count);
+
+            string s1 = list.Find(x => x[0] == 'A');
+            Console.WriteLine("Primeira ocorrencia de nome com a primeira letra A: " + s1);
+
+            string s2 = list.FindLast(x => x[0] == 'A');
+            Console.WriteLine("Ultima ocorrencia de nome iniciado com a letra A: " + s2);
         }
     }
 }
