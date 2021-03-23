67da1aec3303525a9823922cb5b34f58ce6cd0a2
Carlos Machado <carlos.machado.aluno@faculdadepromove.br>
2021-03-23 09:59:08 -0300

    Código comentado exemplificando o uso da Matriz

--- Matrizes/Program.cs
+++ Matrizes/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿//Programa exemplificando algumas funcionalidades de uma matriz
+//A Matriz deste exercício terá o mesmo número de linhas e colunas
+
+using System;
 
 namespace Matrizes
 {
@@ -6,22 +9,30 @@ namespace Matrizes
     {
         static void Main(string[] args)
         {
+            //Recebe a quantidade de linhas e colunas que a matriz terá
             int quantidade = int.Parse(Console.ReadLine());
 
+            //Cria e instancia a Matriz passando a quantidade lida anteriormente como a quantidade
+            //de linhas e colunas
             int[,] mat = new int[quantidade, quantidade];
 
+            //Loop para percorrer as linhas
             for (int i = 0; i < quantidade; i++)
             {
+                //Vetor para armazenar os números informados em uma linha
                 string[] valores = Console.ReadLine().Split(' ');
 
+                //Loop para percorrer as colunas
                 for (int j = 0; j < quantidade; j++)
-                {
+                {   
+                    //Armazena os números do vetor nas colunas de cada linha
                     mat[i, j] = int.Parse(valores[j]);
                 }
             }
 
             Console.WriteLine("Principal diagonal: ");
 
+            //Exibe os números na coluna principal, ou seja, na direção diagonal da matriz
             for (int i = 0; i < quantidade; i++)
             {
                 Console.Write(mat[i, i] + " ");
@@ -30,10 +41,14 @@ namespace Matrizes
 
             int contador = 0;
             string numeros = "";
+            //Loop para percorrer novamente as linhas
             for (int i = 0; i < quantidade; i++)
             {
+                //Loop para percorrer novamente as colunas
                 for (int j = 0; j < quantidade; j++)
                 {
+                    //Verifica se o número é negativo, se veradadeiro adiciona a string e ao contador para
+                    //ser impresso futuramente
                     if (mat[i, j] < 0)
                     {
                         numeros = numeros + " " + mat[i, j];
