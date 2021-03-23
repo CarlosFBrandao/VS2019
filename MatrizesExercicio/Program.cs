9c29bddf02d33d3d9d1a36b8513f05426b2e0add
Carlos Machado <carlos.machado.aluno@faculdadepromove.br>
2021-03-23 11:00:33 -0300

    Exercício de Fixação Matrizes

---
+++ MatrizesExercicio/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace MatrizesExercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Informe a quantidade de linhas e colunas da Matriz: ");
+
+            string[] valores = Console.ReadLine().Split(' ');
+
+            int linhas = int.Parse(valores[0]);
+            int colunas = int.Parse(valores[1]);
+
+            int[,] mat = new int[linhas, colunas];
+
+            for (int i = 0; i < linhas; i++)
+            {
+                Console.WriteLine("Informe os números da linha: " + i);
+                string[] numeros = Console.ReadLine().Split(' ');
+                for (int j = 0; j < colunas; j++)
+                {
+                    mat[i, j] = int.Parse(numeros[j]);
+                }
+            }
+
+            Console.Write("Informe o número que deseja buscar: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < linhas; i++)
+            {
+                for (int j = 0; j < colunas; j++)
+                {
+                    if (mat[i, j] == numero)
+                    {
+                        Console.WriteLine("Posição: " + i + ", " + j);
+                        if (j > 0)
+                        {
+                            Console.WriteLine("Direita: " + mat[i, j - 1]);
+                        }
+                        if (i > 0)
+                        {
+                            Console.WriteLine("Encima: " + mat[i - 1, j]);
+                        }
+                        if (j < linhas - 1)
+                        {
+                            Console.WriteLine("Esquerda: " + mat[i, j + 1]);
+                        }
+                        if (i < colunas - 1)
+                        {
+                            Console.WriteLine("Embaixo: " + mat[i + 1, j]);
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+}
