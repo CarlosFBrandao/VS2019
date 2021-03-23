98cd8d38fb636bc3fafeee187e6523d67d37d3ff
Carlos Machado <carlos.machado.aluno@faculdadepromove.br>
2021-03-23 09:06:33 -0300

    Registro de Funcionários

---
+++ RegistroFuncionario/Funcionario.cs
@@ -0,0 +1,32 @@
+﻿using System.Globalization;
+
+namespace RegistroFuncionario
+{
+    class Funcionario
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public double Salario { get; private set; }
+
+        public Funcionario(int id, string nome, double salario)
+        {
+            Id = id;
+            Nome = nome;
+            Salario = salario;
+        }
+
+        public void AlteraSalario(double porcentagem)
+        {
+            Salario += Salario + porcentagem / 100;
+        }
+
+        public override string ToString()
+        {
+            return Id
+                + ", "
+                + Nome
+                + ", "
+                + Salario.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ RegistroFuncionario/Principal.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace RegistroFuncionario
+{
+    class Principal
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Quantos funcionários deseja adicionar? ");
+            int quantidade = int.Parse(Console.ReadLine());
+
+            List<Funcionario> funcionario = new List<Funcionario>();
+
+            for (int i = 0; i < quantidade; i++)
+            {
+                Console.WriteLine("Funcionário #" + i + ":");
+
+                Console.Write("Informe o ID do Funcionário: ");
+                int id = int.Parse(Console.ReadLine());
+
+                Console.Write("Informe o nome do Funcionário: ");
+                string nome = Console.ReadLine();
+
+                Console.Write("informe o Salário deste funcionário: ");
+                double salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                funcionario.Add(new Funcionario(id, nome, salario));
+            }
+
+            Console.Write("Informe o ID do funcionário que deseja aumentar o salário: ");
+            int buscarId = int.Parse(Console.ReadLine());
+
+            Funcionario emp = funcionario.Find(x => x.Id == buscarId);
+            if (emp != null)
+            {
+                Console.WriteLine("Informe o percentual que será aumentado: ");
+                double percentual = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                emp.AlteraSalario(percentual);
+            }
+            else
+            {
+                Console.WriteLine("ID não encontrado!");
+            }
+
+            Console.WriteLine("==============================");
+            Console.WriteLine("Lista de funcionários atualizados:");
+
+            foreach (Funcionario obj in funcionario)
+            {
+                Console.WriteLine(obj);
+            }
+        }
+    }
+}
