fda2c776e8abe42f4d052aee041b00d28c0af15a
Carlos Machado <carlos.machado.aluno@faculdadepromove.br>
2021-04-01 18:56:19 -0300

    Fundamentos orientada a objeto

---
+++ Employees/Entities/OutsourcedEmployee.cs
@@ -0,0 +1,23 @@
+﻿namespace Employees.Entities
+{
+    class OutsourcedEmployee : Employee
+    {
+        public double AdditionalCharg { get; set; }
+
+        public OutsourcedEmployee()
+        {
+
+        }
+
+        public OutsourcedEmployee(string name, int hours, double valuePerHour, double additionalCharg)
+            : base(name, hours, valuePerHour)
+        {
+            AdditionalCharg = additionalCharg;
+        }
+
+        public override double Payment()
+        {
+            return base.Payment() + AdditionalCharg * 1.1;
+        }
+    }
+}
